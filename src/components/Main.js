6db5d2d34d6b1acc616f836774b098bdcc8eb31f
abbr <wenfred@gmail.com>
2015-12-07 12:26:40 -0800

    tried out bs navbar.

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,11 +10,62 @@ let yeomanImage = require('../images/yeoman.png');
 class AppComponent extends React.Component {
   render() {
     return (
+      <div><nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Brand</a>
+          </div>
+
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+              <li><a href="#">Link</a></li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" className="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li role="separator" className="divider"></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form className="navbar-form navbar-left" role="search">
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Search"/>
+              </div>
+              <button type="submit" className="btn btn-default">Submit</button>
+            </form>
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="#">Link</a></li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" className="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>{/*<!-- /.navbar-collapse -->*/}
+        </div>{/*<!-- /.container-fluid -->*/}
+      </nav>
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <button type="button" className="btn btn-warning">Default</button>
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      </div></div>
     );
   }
 }
