03e688d1842fc9888ae6bae4ec5446383b9aa60a
abbr <wenfred@gmail.com>
2015-12-13 11:40:10 -0800

    created component buttons.

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import lang from 'i18n/lang'
 module.exports = React.createClass({
   render: () =>
     <nav className="navbar navbar-default showpreper-header">
@@ -14,29 +15,22 @@ module.exports = React.createClass({
           <a className="navbar-brand" href="#">ShowPreper</a>
         </div>
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav">
-            <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
-            <li className="dropdown">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                 aria-expanded="false">Dropdown <span className="caret"></span></a>
-              <ul className="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" className="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li role="separator" className="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <form className="navbar-form navbar-left" role="search">
-            <div className="form-group">
-              <input type="text" className="form-control" placeholder="Search"/>
-            </div>
-            <button type="submit" className="btn btn-default">Submit</button>
-          </form>
+          <div className="nav navbar-btn btn-group navbar-left" role="group">
+            {[
+              {icon: 'glyphicon-text-width', text: lang.text, type: 'text'},
+              {icon: 'glyphicon-picture', text: lang.image, type: 'image'},
+              {icon: 'glyphicon-film', text: lang.video, type: 'video'},
+              {icon: 'glyphicon-globe', text: lang.website, type: 'website'},
+              {icon: 'glyphicon-star', text: lang.shapes, type: 'shapes'},
+            ].map(e =>
+              <button type="button" className="btn btn-default" key={e.type}>
+                <span className={'glyphicon ' + e.icon}/>
+                <div className="btn-label">
+                  {e.text}
+                </div>
+              </button>
+            )}
+          </div>
           <ul className="nav navbar-nav navbar-right">
             <li><a href="#">Link</a></li>
             <li className="dropdown">
