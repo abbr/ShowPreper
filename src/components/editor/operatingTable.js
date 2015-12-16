aac793e70e21b4761c262062dc1b0f74eea5523e
abbr <wenfred@gmail.com>
2015-12-16 15:47:45 -0800

    changed style name prefix

--- src/components/editor/operatingTable.js
+++ src/components/editor/operatingTable.js
@@ -1,87 +1,87 @@
-'use strict';
+'use strict'
 
-var React = require('react');
-var Geometry = require('math/Geometry');
-var _ = require('lodash');
+let React = require('react')
+let Geometry = require('math/geometry')
+let _ = require('lodash')
 
-require('components/editor/operatingTable.less');
+require('components/editor/operatingTable.less')
 import ReactDOM from 'react-dom'
 
-var OperatingTable = React.createClass({
-	_computeOtsSquare: function() {
-		var deck = this.props.deck;
-		var slideWidth = deck.config.slideWidth;
-		var slideHeight = deck.config.slideHeight;
+let OperatingTable = React.createClass({
+  _computeOtsSquare: function () {
+    let deck = this.props.deck
+    let slideWidth = deck.config.slideWidth
+    let slideHeight = deck.config.slideHeight
 
-		var rootElSize = window.getComputedStyle(ReactDOM.findDOMNode(this));
+    let rootElSize = window.getComputedStyle(ReactDOM.findDOMNode(this))
 
-		if (!rootElSize)
-			return {};
+    if (!rootElSize)
+      return {}
 
-		var width = parseInt(rootElSize.width);
-		var height = parseInt(rootElSize.height);
+    let width = parseInt(rootElSize.width)
+    let height = parseInt(rootElSize.height)
 
-		var scale = Geometry.getFitSquareScaleFactor(
-			slideWidth,
-			slideHeight,
-			width,
-			height - 20
-		);
+    let scale = Geometry.getFitSquareScaleFactor(
+      slideWidth,
+      slideHeight,
+      width,
+      height - 20
+    )
 
-		var leftOffset = (width - slideWidth * scale) / 2;
-		var topOffset = (height - slideHeight * scale) / 2;
+    let leftOffset = (width - slideWidth * scale) / 2
+    let topOffset = (height - slideHeight * scale) / 2
 
-		return [
-			scale,
-			{
-				transform: 'scale(' + scale + ')',
-				marginLeft: leftOffset + 'px',
-				width: slideWidth,
-				height: slideHeight
-			}
-		];
-	},
+    return [
+      scale,
+      {
+        transform: 'scale(' + scale + ')',
+        marginLeft: leftOffset + 'px',
+        width: slideWidth,
+        height: slideHeight
+      }
+    ]
+  },
 
-	_resized: function() {
-		var style = this._computeOtsSquare();
-		this.setState({
-			otsStyle: style[1],
-			scale: style[0]
-		});
-	},
+  _resized: function () {
+    let style = this._computeOtsSquare()
+    this.setState({
+      otsStyle: style[1],
+      scale: style[0]
+    })
+  },
 
-	onClick() {
-		var slide = this.props.deck.getSelectedSlide();
-		if (slide) {
-			slide.unselectComponents();
-			slide.stopEditingComponents();
-		}
-	},
+  onClick() {
+    let slide = this.props.deck.getSelectedSlide()
+    if (slide) {
+      slide.unselectComponents()
+      slide.stopEditingComponents()
+    }
+  },
 
-	getInitialState: function() {
-		return {};
-	},
+  getInitialState: function () {
+    return {}
+  },
 
-	componentDidMount: function() {
-		this._resized();
+  componentDidMount: function () {
+    this._resized()
 
-		window.addEventListener('resize', this._resized);
-	},
+    window.addEventListener('resize', this._resized)
+  },
 
-	componentWillUnmount: function() {
-		window.removeEventListener('resize', this._resized);
-	},
+  componentWillUnmount: function () {
+    window.removeEventListener('resize', this._resized)
+  },
 
-	render: function() {
-		return (
-			<div
-				className="strt-operating-table"
-				onClick={this.onClick}>
-				<div className="strt-ot-slide" style={this.state.otsStyle}>
-				</div>
-			</div>
-		);
-	}
-});
+  render: function () {
+    return (
+      <div
+        className="sp-operating-table"
+        onClick={this.onClick}>
+        <div className="sp-ot-slide" style={this.state.otsStyle}>
+        </div>
+      </div>
+    )
+  }
+})
 
-module.exports = OperatingTable;
+module.exports = OperatingTable
--- src/math/Geometry.js
+++ src/math/Geometry.js
@@ -1,50 +1,50 @@
-'use strict';
+'use strict'
 
 module.exports = {
-	// TODO: may want to make the square larger even...
-	fitSquare: function(desiredWidth, desiredHeight, width, height) {
-		var finalWidth = desiredWidth;
-		var finalHeight = desiredHeight;
+  // TODO: may want to make the square larger even...
+  fitSquare: function (desiredWidth, desiredHeight, width, height) {
+    var finalWidth = desiredWidth
+    var finalHeight = desiredHeight
 
-		if (finalWidth > width) {
-			var scale = width / finalWidth;
-			finalWidth = width * scale;
-			finalHeight = height * scale;
-		}
+    if (finalWidth > width) {
+      var scale = width / finalWidth
+      finalWidth = width * scale
+      finalHeight = height * scale
+    }
 
-		if (finalHeight > height) {
-			var scale =  height / finalHeight;
-			finalHeight = height * scale;
-			finalWidth = width * scale;
-		}
+    if (finalHeight > height) {
+      var scale = height / finalHeight
+      finalHeight = height * scale
+      finalWidth = width * scale
+    }
 
-		return {
-			width: finalWidth,
-			height: finalHeight
-		};
-	},
+    return {
+      width: finalWidth,
+      height: finalHeight
+    }
+  },
 
-	getFitSquareScaleFactor: function(desiredWidth, desiredHeight, width, height) {
-		var xScale = width / desiredWidth;
-		var yScale = height  / desiredHeight;
+  getFitSquareScaleFactor: function (desiredWidth, desiredHeight, width, height) {
+    var xScale = width / desiredWidth
+    var yScale = height / desiredHeight
 
-		var newHeight = desiredHeight * xScale;
-		if (newHeight > height) {
-			var scale = yScale;
-		} else {
-			var scale = xScale;
-		}
+    var newHeight = desiredHeight * xScale
+    if (newHeight > height) {
+      var scale = yScale
+    } else {
+      var scale = xScale
+    }
 
-		return scale;
-	},
+    return scale
+  },
 
-	centerSquare: function(innerWidth, innerHeight, outerWidth, outerHeight) {
-		var left = (outerWidth - innerWidth) / 2;
-		var top = (outerHeight - innerHeight) / 2;
+  centerSquare: function (innerWidth, innerHeight, outerWidth, outerHeight) {
+    var left = (outerWidth - innerWidth) / 2
+    var top = (outerHeight - innerHeight) / 2
 
-		return {
-			top: top,
-			left: left
-		};
-	}
+    return {
+      top: top,
+      left: left
+    }
+  }
 }
