159887df5ead984c2e3eea30a66a2d96d8dc93ac
abbr <wenfred@gmail.com>
2015-12-19 14:25:12 -0800

    display components in well slides

--- src/components/editor/well/WellSlide.js
+++ src/components/editor/well/WellSlide.js
@@ -1,19 +1,27 @@
 'use strict'
 
 var React = require('react')
-
+var ComponentViewFactory = require('components/widgets/componentViewFactory')
 var WellSlide = React.createClass({
   _clicked: function () {
     this.props.onSlideClicked(this.props.index)
   },
-
   render: function () {
+    let componentsView = this.props.model.components.map((component, index) => {
+      let ComponentView = ComponentViewFactory(component)
+      return (
+        <ComponentView {...component}
+          key = {index}
+        />
+      )
+    })
     return (
       <div
         className={
 					"sp-well-slide " + (this.props.model.selected? "selected":'')
 				}
         onClick={this._clicked}>
+        {componentsView}
       </div>
     )
   }
