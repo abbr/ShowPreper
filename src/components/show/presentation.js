d8a8d6e5ff0519938a75f05e29692f099a823110
abbr <wenfred@gmail.com>
2016-01-02 15:05:21 -0800

    Imported impress.js

--- src/components/show/presentation.js
+++ src/components/show/presentation.js
@@ -1,7 +1,9 @@
 'use strict'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'impress.js'
 import './impress.less'
+import DeckStore from 'stores/deck'
 
 let Presentation = React.createClass({
   render: function () {
