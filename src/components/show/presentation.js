ca5ce50f88bdeee36f6da202063cfad7c07daf67
abbr <wenfred@gmail.com>
2016-01-02 15:11:30 -0800

    render fallback-message inside jsx.

--- src/components/show/presentation.js
+++ src/components/show/presentation.js
@@ -8,7 +8,13 @@ import DeckStore from 'stores/deck'
 let Presentation = React.createClass({
   render: function () {
     return <div>
-      todo</div>
+      <div className="fallback-message">
+        <p>Your browser <b>doesn't support the features required</b> by impress.js, so you are presented with a
+          simplified version of this presentation.</p>
+        <p>For the best experience please use the latest <b>Chrome</b>, <b>Safari</b> or <b>Firefox</b> browser.</p>
+      </div>
+      todo
+    </div>
   }
 })
 // Render the main component into the dom
