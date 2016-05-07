cca460465f976693d02e25613d5e7e779fe5823c
abbr <wenfred@gmail.com>
2016-05-07 11:30:08 -0700

    Update saveAs filename after a opening a different local file.

--- src/components/file/saveAs.js
+++ src/components/file/saveAs.js
@@ -3,6 +3,11 @@ import React from 'react'
 import lang from 'i18n/lang'
 
 let FileSaveAs = React.createClass({
+  getInitialState: function () {
+    return {
+      fn: this.props.deck._fn
+    }
+  },
   onChange: function (e) {
     let newName = e.target.value + '.spj'
     if (newName !== this.props.deck._fn && Object.keys(localStorage).indexOf(newName) >= 0) {
@@ -14,10 +19,18 @@ let FileSaveAs = React.createClass({
     else {
       this.refs.errorMsg.innerHTML = ''
     }
+    this.setState({fn: newName})
   },
   onOk: function () {
     // TODO: delete duplicated file if exists
     this.props.onNewDeck(this.refs.fname.value + '.spj', this.props.deck)
+  },
+  componentWillReceiveProps: function(nextProps) {
+    if (nextProps.deck._fn !== this.props.deck._fn) {
+      this.setState({
+        fn: nextProps.deck._fn
+      })
+    }
   },
   render: function () {
     return <div id="fileSaveAs" className="sp-modal-dialog">
@@ -25,7 +38,7 @@ let FileSaveAs = React.createClass({
         <a href="#close" title="Close" className="sp-modal-close">X</a>
         <h2>Save As ...</h2>
         <input type="text" name="fname" onChange={this.onChange}
-               defaultValue={this.props.deck._fn.replace(/\.spj$/,'')} ref="fname"></input>.spj
+               value={this.state.fn.replace(/\.spj$/,'')} ref="fname"></input>.spj
         <div style={{color: 'red'}} ref="errorMsg"></div>
         <a
           className="btn btn-primary"
