5d424fa043c3fad34cf841ce2d27b1cc635255c5
abbr <wenfred@gmail.com>
2016-05-07 15:40:21 -0700

    handle err message using state

--- src/components/file/saveAs.js
+++ src/components/file/saveAs.js
@@ -5,30 +5,31 @@ import lang from 'i18n/lang'
 let FileSaveAs = React.createClass({
   getInitialState: function () {
     return {
-      fn: this.props.deck._fn
+      fn: this.props.deck._fn,
+      errMsg: ''
     }
   },
   onChange: function (e) {
     let newName = e.target.value + '.spj'
     if (newName !== this.props.deck._fn && Object.keys(localStorage).indexOf(newName) >= 0) {
-      this.refs.errorMsg.innerHTML = lang.duplicatedFileNameErr
+      this.setState({errMsg: lang.duplicatedFileNameErr})
     }
     else if (newName.length === 4) {
-      this.refs.errorMsg.innerHTML = lang.emptyFileNameErr
+      this.setState({errMsg: lang.emptyFileNameErr})
     }
     else {
-      this.refs.errorMsg.innerHTML = ''
+      this.setState({errMsg: ''})
     }
     this.setState({fn: newName})
   },
   onOk: function () {
-    // TODO: delete duplicated file if exists
     this.props.onNewDeck(this.refs.fname.value + '.spj', this.props.deck)
   },
-  componentWillReceiveProps: function(nextProps) {
+  componentWillReceiveProps: function (nextProps) {
     if (nextProps.deck._fn !== this.props.deck._fn) {
       this.setState({
-        fn: nextProps.deck._fn
+        fn: nextProps.deck._fn,
+        errMsg: ''
       })
     }
   },
@@ -39,7 +40,7 @@ let FileSaveAs = React.createClass({
         <h2>Save As ...</h2>
         <input type="text" name="fname" onChange={this.onChange}
                value={this.state.fn.replace(/\.spj$/,'')} ref="fname"></input>.spj
-        <div style={{color: 'red'}} ref="errorMsg"></div>
+        <div style={{color: 'red'}}>{this.state.errMsg}</div>
         <a
           className="btn btn-primary"
           onClick={this.onOk}
