3c8e881ec51d8290727a538f3af7b37c6c7e8fcd
abbr <wenfred@gmail.com>
2016-06-05 11:52:31 -0700

    set default widget to TextBox

--- src/components/widgets/widgetFactory.js
+++ src/components/widgets/widgetFactory.js
@@ -3,11 +3,12 @@ import './widgets.less'
 var constructors = {
   TextBox: require('./primitives/textBox')
   , Slide: require('./primitives/slide')
+  , default: require('./primitives/textBox')
 }
 
 let WidgetFactory = function (component) {
   let name = component.type || component
-  return constructors[name]
+  return constructors[name] || constructors['default']
 }
 
 module.exports = WidgetFactory
