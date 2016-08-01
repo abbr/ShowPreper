fd07de9b44c4d5232c64239439d7b1fb9f4eb722
abbr <wenfred@gmail.com>
2016-08-01 11:37:34 -0700

    added palettes store

---
+++ src/stores/palettes.js
@@ -0,0 +1,23 @@
+'use strict'
+import _ from 'lodash'
+import DefaultPalettes from 'sources/default_palettes.json'
+
+const _spDefaultFileNm = 'palettes.json'
+let Palettes = function () {
+  let savedPalettes
+  if (typeof(Storage) !== "undefined") {
+    try {
+      savedPalettes = JSON.parse(localStorage.getItem(_spDefaultFileNm))
+    }
+    catch (ex) {
+    }
+  }
+  _.assign(this, savedPalettes || _.cloneDeep(DefaultPalettes))
+}
+
+Palettes.prototype.save = function () {
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem(_spDefaultFileNm, JSON.stringify(this))
+  }
+}
+export default Palettes
