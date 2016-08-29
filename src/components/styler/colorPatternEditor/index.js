3d9e9efbfa59688e28d6a182f72e71d012895010
abbr <wenfred@gmail.com>
2016-08-28 18:54:36 -0700

    use accordion to display color patterns

--- src/components/styler/colorPatternEditor/index.js
+++ src/components/styler/colorPatternEditor/index.js
@@ -31,33 +31,70 @@ export default React.createClass({
       }
     }
     return <div id="sp-color-pattern-editor">
-      <input type="radio" onChange={(evt)=> {
-        this.props.updateStyle({background: 'rgb()'})
-      }}
-             checked={type === 'color'}/>color:
-      <input id='colorpicker'/>
-      <p/>
-      <input type="radio" onChange={(evt)=> {
-        this.props.updateStyle({background: 'linear-gradient()'})
-      }}
-             checked={type === 'linear-gradient'}/>linear gradient
-      <p/>
-      <input type="radio" onChange={(evt)=> {
-        this.props.updateStyle({background: 'radio-gradient()'})
-      }}
-             checked={type === 'radio-gradient'}/>radio gradient
-      <h1>
-        <svg width="32" height="32" fill="red">
-          <defs>
-            <filter id="f1" x="0" y="0" width="150%" height="150%">
-              <feOffset result="offOut" in="SourceAlpha" dx="3" dy="3"/>
-              <feGaussianBlur result="blurOut" in="offOut" stdDeviation="2"/>
-              <feBlend in="SourceGraphic" in2="blurOut" mode="normal"/>
-            </filter>
-          </defs>
-          <path id="marker-up" filter="url(#f1)" d="M 8 0 L 16 16 L 0 16 z"></path>
-        </svg>
-      </h1>
+      <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <div className="panel panel-default">
+          <div className="panel-heading" role="tab" id="headingOne">
+            <h4 className="panel-title">
+              <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
+                 aria-controls="collapseOne">
+                <input type="radio" onChange={(evt)=> {
+                  this.props.updateStyle({background: 'rgb()'})
+                }} checked={type === 'color'}></input>color:
+              </a>
+            </h4>
+          </div>
+          <div id="collapseOne" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+            <div className="panel-body">
+              <input id='colorpicker'/>
+            </div>
+          </div>
+        </div>
+        <div className="panel panel-default">
+          <div className="panel-heading" role="tab" id="headingTwo">
+            <h4 className="panel-title">
+              <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"
+                 aria-expanded="false" aria-controls="collapseTwo">
+                <input type="radio" onChange={(evt)=> {
+                  this.props.updateStyle({background: 'linear-gradient()'})
+                }}
+                       checked={type === 'linear-gradient'}/>linear gradient
+              </a>
+            </h4>
+          </div>
+          <div id="collapseTwo" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+            <div className="panel-body">2
+            </div>
+          </div>
+        </div>
+        <div className="panel panel-default">
+          <div className="panel-heading" role="tab" id="headingThree">
+            <h4 className="panel-title">
+              <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                 href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <input type="radio" onChange={(evt)=> {
+                  this.props.updateStyle({background: 'radio-gradient()'})
+                }}
+                       checked={type === 'radio-gradient'}/>radio gradient
+
+              </a>
+            </h4>
+          </div>
+          <div id="collapseThree" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+            <div className="panel-body">
+              <svg width="32" height="32" fill="red">
+                <defs>
+                  <filter id="f1" x="0" y="0" width="150%" height="150%">
+                    <feOffset result="offOut" in="SourceAlpha" dx="3" dy="3"/>
+                    <feGaussianBlur result="blurOut" in="offOut" stdDeviation="2"/>
+                    <feBlend in="SourceGraphic" in2="blurOut" mode="normal"/>
+                  </filter>
+                </defs>
+                <path id="marker-up" filter="url(#f1)" d="M 8 0 L 16 16 L 0 16 z"></path>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   }
 })
