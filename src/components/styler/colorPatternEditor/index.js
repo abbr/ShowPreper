cb1f0ebb360c4ec372a80d362f8d9b0c2399a8d6
abbr <wenfred@gmail.com>
2016-08-28 20:07:22 -0700

    use accordion to display color patterns

--- src/components/styler/colorPatternEditor/index.js
+++ src/components/styler/colorPatternEditor/index.js
@@ -21,7 +21,7 @@ export default React.createClass({
     let type
     if (this.props.currentStyle) {
       if (this.props.currentStyle.match(/^radial-gradient/)) {
-        type = 'radio-gradient'
+        type = 'radial-gradient'
       }
       else if (this.props.currentStyle.match(/^linear-gradient/)) {
         type = 'linear-gradient'
@@ -35,15 +35,18 @@ export default React.createClass({
         <div className="panel panel-default">
           <div className="panel-heading" role="tab" id="headingOne">
             <h4 className="panel-title">
+              <input type="radio" readOnly checked={type === 'color'}></input>
               <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
-                 aria-controls="collapseOne">
-                <input type="radio" onChange={(evt)=> {
-                  this.props.updateStyle({background: 'rgb()'})
-                }} checked={type === 'color'}></input>color:
+                 aria-controls="collapseOne" onClick={(evt)=> {
+                this.props.updateStyle({background: 'rgb()'})
+                return true
+              }}>
+                color
               </a>
             </h4>
           </div>
-          <div id="collapseOne" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+          <div id="collapseOne" className="panel-collapse collapse"
+               role="tabpanel" aria-labelledby="headingOne">
             <div className="panel-body">
               <input id='colorpicker'/>
             </div>
@@ -52,16 +55,17 @@ export default React.createClass({
         <div className="panel panel-default">
           <div className="panel-heading" role="tab" id="headingTwo">
             <h4 className="panel-title">
+              <input type="radio" readOnly checked={type === 'linear-gradient'}/>
               <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"
-                 aria-expanded="false" aria-controls="collapseTwo">
-                <input type="radio" onChange={(evt)=> {
-                  this.props.updateStyle({background: 'linear-gradient()'})
-                }}
-                       checked={type === 'linear-gradient'}/>linear gradient
+                 aria-expanded="false" aria-controls="collapseTwo" onClick={(evt)=> {
+                this.props.updateStyle({background: 'linear-gradient()'})
+              }}>
+                linear gradient
               </a>
             </h4>
           </div>
-          <div id="collapseTwo" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+          <div id="collapseTwo" className="panel-collapse collapse"
+               role="tabpanel" aria-labelledby="headingTwo">
             <div className="panel-body">2
             </div>
           </div>
@@ -69,17 +73,17 @@ export default React.createClass({
         <div className="panel panel-default">
           <div className="panel-heading" role="tab" id="headingThree">
             <h4 className="panel-title">
+              <input type="radio" readOnly checked={type === 'radial-gradient'}/>
               <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
-                 href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                <input type="radio" onChange={(evt)=> {
-                  this.props.updateStyle({background: 'radio-gradient()'})
-                }}
-                       checked={type === 'radio-gradient'}/>radio gradient
-
+                 href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" onClick={(evt)=> {
+                this.props.updateStyle({background: 'radial-gradient()'})
+              }}>radial gradient
               </a>
             </h4>
           </div>
-          <div id="collapseThree" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+          <div id="collapseThree"
+               className="panel-collapse collapse" role="tabpanel"
+               aria-labelledby="headingThree">
             <div className="panel-body">
               <svg width="32" height="32" fill="red">
                 <defs>
