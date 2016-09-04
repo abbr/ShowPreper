c093b41afe20267569f411a80a85bc79af869f9b
abbr <wenfred@gmail.com>
2016-09-04 12:11:50 -0700

    fix problem component not selectable.

--- src/components/widgets/displayableComponent.js
+++ src/components/widgets/displayableComponent.js
@@ -5,29 +5,29 @@ let _ = require('lodash')
 
 let DisplayableComponent = React.createClass({
   render: function () {
-    const {component, ...rest} = this.props
+    const {component, container, onSelectedWidgetUpdated, idx, onScaleMouseDown, onRotateMouseDown, onKillMouseDown, setDraggable, editable, componentStyle, ...rest} = this.props
     const WidgetFactory = require('./widgetFactory')
     let Widget = WidgetFactory(component)
 
-    let widgetStyle = _.merge({transform: ''}, this.props.componentStyle || {}), componentStyle = {transform: ''}
+    let widgetStyle = _.merge({transform: ''}, componentStyle || {}), thisComponentStyle = {transform: ''}
 
     let translate3D = {}
     translate3D.x = component.x || 0
     translate3D.y = component.y || 0
     translate3D.z = component.z || 0
-    componentStyle.transform += ' translate3d(' + translate3D.x + 'px,' + translate3D.y + 'px,' + translate3D.z + 'px)'
-    component.width && (componentStyle.width = component.width)
-    component.height && (componentStyle.height = component.height)
+    thisComponentStyle.transform += ' translate3d(' + translate3D.x + 'px,' + translate3D.y + 'px,' + translate3D.z + 'px)'
+    component.width && (thisComponentStyle.width = component.width)
+    component.height && (thisComponentStyle.height = component.height)
     let scaleX = (component.scale && component.scale.x) || 1
     let scaleY = (component.scale && component.scale.y) || 1
-    componentStyle.transform += " scale(" + scaleX + "," + scaleY + ")"
+    thisComponentStyle.transform += " scale(" + scaleX + "," + scaleY + ")"
     if (component.rotate) {
       let rotStr = ''
       component.rotate.x && (rotStr += " rotateX(" + component.rotate.x + "rad)")
       component.rotate.y && (rotStr += " rotateY(" + component.rotate.y + "rad)")
       component.rotate.z && (rotStr += " rotateZ(" + component.rotate.z + "rad)")
       if (this.props.combinedTransform) {
-        componentStyle.transform += rotStr
+        thisComponentStyle.transform += rotStr
       }
       else {
         widgetStyle.transform += rotStr
@@ -38,7 +38,7 @@ let DisplayableComponent = React.createClass({
       component.skew.y && (rotStr += " skewY(" + component.skew.y + "rad)")
       component.skew.x && (rotStr += " skewX(" + component.skew.x + "rad)")
       if (this.props.combinedTransform) {
-        componentStyle.transform += rotStr
+        thisComponentStyle.transform += rotStr
       }
       else {
         widgetStyle.transform += rotStr
@@ -47,14 +47,14 @@ let DisplayableComponent = React.createClass({
     if (widgetStyle.transform === '') {
       delete widgetStyle.transform
     }
-    if (componentStyle.transform === '') {
-      delete componentStyle.transform
+    if (thisComponentStyle.transform === '') {
+      delete thisComponentStyle.transform
     }
     return (
       <div
-        {...this.rest}
+        {...rest}
         className={classNames("sp-component", this.props.ownClassName, this.props.className)}
-        style={componentStyle}>
+        style={thisComponentStyle}>
         <div
           className={this.props.className}
           style={{height: '100%'}}
