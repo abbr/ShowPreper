6db31c32ed7651ea3dc04cb947fd1d97019e83d2
abbr <wenfred@gmail.com>
2016-09-05 16:06:37 -0700

    dynamically changing svg filter doesn't get refreshed in either chrome nor Edge.  But changing xlink:href do.

--- src/components/styler/colorPatternEditor/marker.js
+++ src/components/styler/colorPatternEditor/marker.js
@@ -3,22 +3,30 @@ export default React.createClass({
   render: function () {
     return (
       <svg className="sp-gradient-marker" xmlns="http://www.w3.org/2000/svg"
-           width="32" height="32" fill="red"
+           width="32" height="32" viewbox="0 0 32 32" fill="red"
            style={{marginLeft: -8, marginBottom: -16}}
            aria-labelledby="title">
         <title>marker</title>
         <defs>
-          <filter id="f1" x="0" y="0" width="150%" height="150%">
-            <feOffset result="offOut" in="SourceAlpha" dx={this.props.pressed ? 1 : 3} dy={this.props.pressed ? 1 : 3}/>
-            <feGaussianBlur result="blurOut" in="offOut" stdDeviation={this.props.pressed ? 1 : 2}/>
+          <filter id="pressed" x="0" y="0" width="150%" height="150%">
+            <feOffset result="offOut" in="SourceAlpha" dx="1" dy="1"/>
+            <feGaussianBlur result="blurOut" in="offOut" stdDeviation="1"/>
             <feBlend in="SourceGraphic" in2="blurOut" mode="normal"/>
           </filter>
-          <path id="up" filter="url(#f1)" d="M 8 0 L 16 16 L 0 16 z"></path>
-          <path id="down" filter="url(#f1)" d="M 0 0 L 16 0 L 8 16 z"></path>
+          <filter id="unpressed" x="0" y="0" width="150%" height="150%">
+            <feOffset result="offOut" in="SourceAlpha" dx="3" dy="3"/>
+            <feGaussianBlur result="blurOut" in="offOut" stdDeviation="2"/>
+            <feBlend in="SourceGraphic" in2="blurOut" mode="normal"/>
+          </filter>
+          <path id="up-pressed" filter="url(#pressed)" d="M 8 0 L 16 16 L 0 16 z"></path>
+          <path id="down-pressed" filter="url(#pressed)" d="M 0 0 L 16 0 L 8 16 z"></path>
+          <path id="up-unpressed" filter="url(#unpressed)" d="M 8 0 L 16 16 L 0 16 z"></path>
+          <path id="down-unpressed" filter="url(#unpressed)" d="M 0 0 L 16 0 L 8 16 z"></path>
         </defs>
-        <use xlinkHref={this.props.down ? '#down' : '#up'} onClick={(evt)=> {
-          this.props.onClick(evt, this)
-        }} style={{cursor: 'pointer'}}></use>
+        <use xlinkHref={(this.props.down ? '#down-' : '#up-') + (this.props.pressed ? 'pressed' : 'unpressed')}
+             onClick={(evt)=> {
+               this.props.onClick(evt, this)
+             }} style={{cursor: 'pointer'}}></use>
       </svg>
     )
   }
