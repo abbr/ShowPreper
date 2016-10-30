676cf9f9cc3193458a2f7dc3bb6e6550dff61da8
abbr <wenfred@gmail.com>
2016-10-30 11:59:48 -0700

    added parse border function

--- src/components/styler/borderEditor/index.js
+++ src/components/styler/borderEditor/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
 export default React.createClass({
+  parseBorder: function () {
+    let border = {width: {}}
+    try {
+      let widthComponentsRaw = this.props.currentStyle.borderWidth.split(' ')
+      border.width.components = widthComponentsRaw.map((e, i)=> {
+        let lengthMatch = e.match(/(\d+)(\w+)/)
+        if (lengthMatch) {
+          return {
+            length: parseInt(lengthMatch[1]),
+            uom: lengthMatch[2]
+          }
+        }
+        else {
+          return e
+        }
+      })
+    }
+    catch (ex) {
+    }
+    return border
+  },
   render: function () {
-    return <div>todo</div>
+    let border = this.parseBorder()
+    return <div>{border.width.components && border.width.components.length}</div>
   }
 })
