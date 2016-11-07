51acf8980b0997e8ac8c13dc73bf272d93205f30
abbr <wenfred@gmail.com>
2016-11-06 16:18:18 -0800

    display border width components

--- src/components/styler/borderEditor/index.js
+++ src/components/styler/borderEditor/index.js
@@ -35,20 +35,39 @@ export default React.createClass({
   },
   onChangeWidthComponentCnt: function (newCnt) {
     let border = this.parseBorder()
-    if (newCnt > border.width.components.length) {
+    let diff = newCnt - border.width.components.length
+    if (diff > 0) {
       let i = 0
-      while (i < (newCnt - border.width.components.length)) {
+      while (i < diff) {
         border.width.components.push({length: 0, uom: 'px'})
         i++
       }
     }
     else {
-      border.width.components.splice(newCnt, border.width.components.length - newCnt)
+      border.width.components.splice(newCnt, -diff)
     }
+    this.props.updateStyle(this.composeBorder(border))
+  },
+  onChangeWidthComponent: function (i, prop, newVal) {
+    let border = this.parseBorder()
+    border.width.components[i][prop] = (newVal instanceof Object) ? newVal.target.value : newVal
     this.props.updateStyle(this.composeBorder(border))
   },
   render: function () {
     let border = this.parseBorder()
+    let borderWidthComponents = border.width.components && border.width.components.map((e, i)=> {
+        return <div className="col-xs-3" key={i}
+                    style={{display: 'inline'}}>
+          <input value={e.length}
+                 size={1}
+                 onChange={this.onChangeWidthComponent.bind(null, i, 'length')
+                 }/>
+          <DropdownList data={['px', 'em']}
+                        value={e.uom}
+                        onChange={this.onChangeWidthComponent.bind(null, i, 'uom')}
+          />
+        </div>
+      })
     return <div className="container-fluid">
       <div className="row">
         <div className="col-xs-1">Width:</div>
@@ -61,8 +80,8 @@ export default React.createClass({
       </div>
       <div className="row">
         <div className="col-xs-1"></div>
-        <div className="col-xs-11">
-          sdfafd
+        <div className="col-xs-11 row">
+          {borderWidthComponents}
         </div>
 
       </div>
