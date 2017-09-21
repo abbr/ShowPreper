bd45ce15c51f89be42b79d710f4f0fd712a0f6c9
abbr <wenfred@gmail.com>
2017-09-20 21:50:18 -0700

    #9 added puppeteer

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,28 @@
-var webpackCfg = require('./webpack.config')
+const webpackCfg = require('./webpack.config')
+const ChromiumRevision = require('puppeteer/package.json').puppeteer
+  .chromium_revision
+const Downloader = require('puppeteer/utils/ChromiumDownloader')
+const revisionInfo = Downloader.revisionInfo(
+  Downloader.currentPlatform(),
+  ChromiumRevision
+)
+
+process.env.CHROME_BIN = revisionInfo.executablePath
 
 module.exports = function(config) {
   config.set({
     basePath: '',
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     files: ['test/loadtests.js'],
     port: 8080,
     captureTimeout: 60000,
-    frameworks: ['phantomjs-shim', 'mocha', 'chai'],
+    frameworks: ['mocha', 'chai'],
     client: {
       mocha: {}
     },
+    colors: true,
     singleRun: true,
+    logLevel: config.LOG_INFO,
     reporters: ['mocha', 'coverage'],
     preprocessors: {
       'test/loadtests.js': ['webpack', 'sourcemap']
@@ -23,6 +34,7 @@ module.exports = function(config) {
     coverageReporter: {
       dir: 'coverage/',
       reporters: [{ type: 'html' }, { type: 'text' }]
-    }
+    },
+    concurrency: Infinity
   })
 }
