477c404173e01f5b655c324904979178af99d03a
abbr <wenfred@gmail.com>
2017-10-22 11:50:10 -0700

    #9 added more initial expectations

--- test/components/show/impressTest.js
+++ test/components/show/impressTest.js
@@ -7,5 +7,7 @@ describe('show > impress', () => {
 
   it('should have 5 initial views', () => {
     expect(appWrapper.find('#impress').props().children.length).to.equal(5)
+    expect($('body').attr('class')).to.contain('impress-on-step-1')
+    expect($('#step-1').attr('class').split(' ')).to.include('active')
   })
 })
